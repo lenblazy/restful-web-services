966e651d2d6a0efec03b671594437643618572b9
lenblazy <lenblazy@gmail.com>
2023-05-30 09:28:50 +0300

    Add post request to create user

--- src/main/java/com/lenibonje/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ src/main/java/com/lenibonje/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -13,23 +13,28 @@ public class UserDaoService {
     // JPA/Hibernate > Database
     // UserDaoService > Static List
 
-    private static List<User> users = new ArrayList<>();
+    private static final List<User> users = new ArrayList<>();
+    private static int usersCount = 0;
 
     static {
-        users.add(new User(1,"Adam", LocalDate.now().minusYears(30)));
-        users.add(new User(2,"Eve",LocalDate.now().minusYears(25)));
-        users.add(new User(3,"Jim",LocalDate.now().minusYears(20)));
+        users.add(new User(++usersCount,"Adam", LocalDate.now().minusYears(30)));
+        users.add(new User(++usersCount,"Eve",LocalDate.now().minusYears(25)));
+        users.add(new User(++usersCount,"Jim",LocalDate.now().minusYears(20)));
     }
 
     public List<User> findAll() {
         return users;
     }
 
-    //public User save(User user) {
-
     public User findOne(int id) {
         Predicate<? super User> predicate = user -> user.getUserId().equals(id);
-        return users.stream().filter(predicate).findFirst().get();
+        return users.stream().filter(predicate).findFirst().orElse(null);
+    }
+
+    public User save(User user) {
+        user.setUserId(++usersCount);
+        users.add(user);
+        return user;
     }
 
 }
--- src/main/java/com/lenibonje/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/lenibonje/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,8 +1,10 @@
 package com.lenibonje.rest.webservices.restfulwebservices.user;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -16,15 +18,19 @@ public class UserResource {
     }
 
     @GetMapping(path = "/users")
-
     public List<User> getUsers() {
         return userDaoService.findAll();
     }
 
     @GetMapping(path = "/users/{id}")
-
     public User retrieveUser(@PathVariable int id) {
         return userDaoService.findOne(id);
+    }
+
+    //POST /users
+    @PostMapping("/users")
+    public void createUser(@RequestBody User user){
+        userDaoService.save(user);
     }
 
 
