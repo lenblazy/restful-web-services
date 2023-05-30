56ad72044e9f934604f3dc0d3d80883b4beafa6d
lenblazy <lenblazy@gmail.com>
2023-05-30 09:43:23 +0300

    Change response status to created 201

--- src/main/java/com/lenibonje/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/lenibonje/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,11 +1,14 @@
 package com.lenibonje.rest.webservices.restfulwebservices.user;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -29,8 +32,13 @@ public class UserResource {
 
     //POST /users
     @PostMapping("/users")
-    public void createUser(@RequestBody User user){
-        userDaoService.save(user);
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        User savedUser = userDaoService.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getUserId())
+                .toUri();
+        return ResponseEntity.created(location).build();
     }
 
 
