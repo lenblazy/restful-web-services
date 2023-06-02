af5cf26a23486f5151317fb748f3bd5875b91db0
lenblazy <lenblazy@gmail.com>
2023-06-02 11:42:20 +0300

    Add i18n for languages

--- src/main/java/com/lenibonje/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/lenibonje/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,15 +1,36 @@
 package com.lenibonje.rest.webservices.restfulwebservices.helloworld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 //REST API
 @RestController
 public class HelloWorldController {
+
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     // /hello-world
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
         return "Hello World";
     }
+
+    @GetMapping(path = "/hello-world-i18n")
+    public String helloWorldi18n() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage(
+                "good.morning.message",
+                null,
+                "Default Message",
+                locale);
+    }
+
 }
