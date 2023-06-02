972ec3d336c2fea7ef5747eba0f829b5e51e4c60
lenblazy <lenblazy@gmail.com>
2023-06-02 10:32:17 +0300

    Implement delete method

--- src/main/java/com/lenibonje/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/lenibonje/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,11 +1,7 @@
 package com.lenibonje.rest.webservices.restfulwebservices.user;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
@@ -32,6 +28,11 @@ public class UserResource {
             throw new UserNotFoundException("id: " + id + " not found");
         }
         return user;
+    }
+
+    @DeleteMapping(path = "/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        userDaoService.deleteById(id);
     }
 
     //POST /users
