b7f5e3fbe6ac07a7c39f1a2fc0a1d8684b3c6334
lenblazy <lenblazy@gmail.com>
2023-06-06 10:29:48 +0300

    Add HATEOAS

--- src/main/java/com/lenibonje/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/lenibonje/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,12 +1,16 @@
 package com.lenibonje.rest.webservices.restfulwebservices.user;
 
 import jakarta.validation.Valid;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @RestController
 public class UserResource {
@@ -23,12 +27,17 @@ public class UserResource {
     }
 
     @GetMapping(path = "/users/{id}")
-    public User retrieveUser(@PathVariable int id) {
+    public EntityModel<User> retrieveUser(@PathVariable int id) {
         User user = userDaoService.findOne(id);
         if(user == null) {
             throw new UserNotFoundException("id: " + id + " not found");
         }
-        return user;
+
+        EntityModel<User> userModel = EntityModel.of(user);
+        WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).getUsers());
+        userModel.add(link.withRel("all-users"));
+
+        return userModel;
     }
 
     @DeleteMapping(path = "/users/{id}")
