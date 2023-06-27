d35a30df9fe410beb151ea8d9efd8f4ed2aa53e1
lenblazy <lenblazy@gmail.com>
2023-06-27 14:39:15 +0300

    Add security config

---
+++ src/main/java/com/lenibonje/rest/webservices/restfulwebservices/security/SpringSecurityConfiguration.java
@@ -0,0 +1,30 @@
+package com.lenibonje.rest.webservices.restfulwebservices.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SpringSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        //1. All requests should be authenticated
+        http.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated()
+        );
+
+        // 2. If a request is not authenticated, show web page
+        http.httpBasic(withDefaults());
+
+        // CSRF -> POST, PUT
+        http.csrf().disable();
+
+        return http.build();
+    }
+
+}
